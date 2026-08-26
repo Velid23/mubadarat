6567fbff730fe85043fd4eeda4ce0a5f50dc56cc
Velid 23 <velid2362006@gmail.com>
2026-08-26 14:25:58 +0300

    clean project update

--- mubadaratproject/next.config.ts
+++ mubadaratproject/next.config.ts
@@ -1,10 +1,7 @@
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // لتصدير الموقع كملفات HTML/CSS/JS جاهزة
-  images: {
-    unoptimized: true, // ضروري لأن GitHub Pages لا يدعم سيرفر معالجة صور Next.js
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
