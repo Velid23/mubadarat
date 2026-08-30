ab07fe21f6f0bceec95abed612fc5014c4986f76
Velid 23 <velid2362006@gmail.com>
2026-08-30 12:09:35 +0300

    fix next image config

--- mubadaratproject/components/home/gallerysection.tsx
+++ mubadaratproject/components/home/gallerysection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import { GalleryItem, ContactInfo } from "@/types";
 
 export default function GallerySection() {
