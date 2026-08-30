e8e50653bd470988c8d6a72571d0298e15cfd6c2
Velid 23 <velid2362006@gmail.com>
2026-08-30 11:58:48 +0300

    fix next image config

--- mubadaratproject/next.config.ts
+++ mubadaratproject/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -10,6 +11,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "**.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "blob.vercel-storage.com",
       },
     ],
   },
